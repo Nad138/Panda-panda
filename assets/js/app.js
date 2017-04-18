c00d5e0879a3c3bced1223eb29d5ec49f2cf26a2
Nadia Cuadros <november138@hotmail.com>
2017-04-18 04:49:32 -0500

    reestructurando las imágenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 var fotos = document.getElementById("fotos");
 for(var i=1;i<5;i++){
+  var figure = document.createElement("figure");
+  var span = document.createElement("span");
+  span.setAttribute("content","x");
   var img = document.createElement("img");
   img.setAttribute("src","assets/img/"+i+".jpg");
-  fotos.appendChild(img);
+  figure.appendChild(img);
+  figure.appendChild(span);
+  fotos.appendChild(figure);
   }
