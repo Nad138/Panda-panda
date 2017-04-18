aa3b3a73a2914b963d7c1d593e55744a78877e26
Nadia Cuadros <november138@hotmail.com>
2017-04-18 05:39:01 -0500

    creando funcion para ocultar elementos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,24 @@
 var fotos = document.getElementById("fotos");
+fotos.setAttribute("display","block");
 for(var i=1;i<5;i++){
   var figure = document.createElement("figure");
   var span = document.createElement("span");
-  span.setAttribute("content","x");
+  span.setAttribute("content","X");
   var img = document.createElement("img");
   img.setAttribute("src","assets/img/"+i+".jpg");
   figure.appendChild(img);
   figure.appendChild(span);
   fotos.appendChild(figure);
   }
+
+  function fotos(){
+    var pandas = document.getElementById("fotos");
+    if(fotos.display=none){
+      fotos.display=inline;
+    }
+    else{
+      fotos.display=none;
+    }
+  }
+  var restaurar =document.getElementById("restaurar");
+  restaurar.addEventListener("onclick", fotos());
